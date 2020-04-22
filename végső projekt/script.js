6b614e940e7ba5523f6c6d6965d0ddff8d1d9136
ITIIS <ITIIS@users.noreply.github.com>
2020-04-22 16:53:00 +0200

    day 3

--- végső projekt/script.js
+++ végső projekt/script.js
@@ -1,11 +1,46 @@
 let auto = {
     photo: 'images/car.jpg',
     title: 'Sportkocsi',
-    description: 'Itt egy Nissan',
+    description: 'Itt egy Nissan GT-R',
 };
-$('#photo').attr('src', data.photo);
+//let city = {
+   // photo: 'images/city.jpg',
+  //title: 'Varos,
+  // description: 'Itt egy varos',
+// };
+
+
+
+ 
+
+
+
+
+//$('#photo').attr('src', data.photo);
 let myImages = [auto];
 let currentPhoto = 0;
 $('#photo').attr('src', myImages[currentPhoto].photo);
 $('#photo-title').text(myImages[currentPhoto].title);
-$('#photo-description').text(myImages[currentPhoto].description);
+$('#photo-description').text(myImages[currentPhoto].description); 
+
+let loadCurrent = (photoNumber) => {
+    $('#photo').attr('src', myImages[currentPhoto].photo);
+    $('#photo-title').text(myImages[currentPhoto].title);
+    $('#photo-description').text(myImages[currentPhoto].description);
+}
+
+$('.gomb1').click(() => {
+  if(currentPhoto > 1) {
+    currentPhoto--;  
+  }
+  loadCurrent(currentp);
+});
+
+$('.gomb2').click(() => {
+  if(currentPhoto < 5) { 
+    currentPhoto++; 
+  }
+  loadCurrent(currentPhoto);
+});
+
+loadCurrent(currentPhoto);
